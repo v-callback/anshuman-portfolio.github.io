9f97210a31b5a9eead22d322198a0e2cf623ec99
Anshuman <55253722+v-callback@users.noreply.github.com>
2023-02-24 00:30:13 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+/*-----------Typing Animation---------*/
+
+var typed = new Typed(".typing",{
+    strings:["Full Stack Developer","Java Programmer","Front-End Developer","Final year Student","Photographer","Web Designer"],
+    typeSpeed:80,
+    BackSpeed:70,
+    loop:true
+})
